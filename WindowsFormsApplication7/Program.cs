e21b15694602a6e4e040667be2533f15eb606c9e
Zammstein <sammeyer1994@hotmail.com>
2014-12-08 12:06:46 +0100

    random

--- WindowsFormsApplication7/Program.cs
+++ WindowsFormsApplication7/Program.cs
@@ -17,6 +17,7 @@ namespace WindowsFormsApplication7
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.Write("a");
         }
     }
 
