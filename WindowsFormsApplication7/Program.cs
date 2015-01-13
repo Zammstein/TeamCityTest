60a5f6b4cd93c7b8afc9596b10eb0718f7b5988d
Zammstein <sammeyer1994@hotmail.com>
2015-01-13 09:52:33 +0100

    Last test pipelines and build

--- WindowsFormsApplication7/Program.cs
+++ WindowsFormsApplication7/Program.cs
@@ -10,7 +10,7 @@ namespace WindowsFormsApplication7
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
